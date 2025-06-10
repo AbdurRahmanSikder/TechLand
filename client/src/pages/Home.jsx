3a26537b27890c87d565b1a0ecc563de728b28b4
Abdur Rahman <19cse040@gmail.com>
2025-06-10 23:09:54 +0600

    vercel json added

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Mainbanner from '../components/Mainbanner'
-import Categories from '../components/categories'
+import Categories from '../components/Categories'
 import BestSeller from '../components/BestSeller'
 import BottomBanner from '../components/BottomBanner'
 import Newsletter from '../components/Newsletter'
