0409256c6cf3413acb12ae0f52b06b1b18a2da46
Abdur Rahman <119144605+AbdurRahmanSikder@users.noreply.github.com>
2025-06-18 10:59:30 +0600

    Update sellerController.js

--- server/controllers/sellerController.js
+++ server/controllers/sellerController.js
@@ -9,6 +9,7 @@ export const sellerLogin = async (req, res) => {
             const token = jwt.sign({ email }, process.env.SELLER_EMAIL, { expiresIn: '7d' });
 
             res.cookie('sellerToken', token, {
+                path: '/',
                 httpOnly: true,
                 secure: process.env.NODE_ENV === 'production',
                 sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
@@ -40,6 +41,7 @@ export const isSellerAuth = async (req, res) => {
 export const logout = async (req, res) => {
     try {
         res.clearCookie('sellerToken', {
+            path: '/',
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
             sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict'
