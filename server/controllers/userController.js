6d4e272c68c01742d4652322a265db1fdd09dd38
Abdur Rahman <119144605+AbdurRahmanSikder@users.noreply.github.com>
2025-06-18 20:02:35 +0600

    Update userController.js

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -91,8 +91,7 @@ export const logout = async (req, res) => {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
             sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
-            path: '/',
-            maxAge: 0,
+            
         })
         return res.json({success:true, message: "Logged Out"})
     }
